208d1242bce793ca179662aa833d58a87f36b4ac
scottmr01 <96456356+scottmr01@users.noreply.github.com>
2022-05-06 12:40:45 -0500

    Update Index Utliization.sql

--- Index Utliization.sql
+++ Index Utliization.sql
@@ -1,8 +1,8 @@
 DECLARE
- @tableName nvarchar(30)=N'some db';
+ @dbName nvarchar(30)=N'some db';
 
 SELECT 
- OBJECT_NAME(S.object_id, DB_ID(@tableName)) AS table_name,
+ CONCAT( OBJECT_SCHEMA_NAME(S.object_id, DB_ID(@dbName)),'.', OBJECT_NAME(S.object_id, DB_ID(@dbName)) )  AS table_name,
  I.name AS index_name,
  s.user_seeks + s.user_scans + s.user_lookups AS "user_reads",
  S.user_seeks,
@@ -13,6 +13,6 @@ FROM sys.dm_db_index_usage_stats AS S
  INNER JOIN sys.indexes AS I
 		 ON S.object_id = I.object_id
 		AND S.index_id = I.index_id
-WHERE S.database_id = DB_ID(@tableName)
+WHERE S.database_id = DB_ID(@dbName)
 
 ORDER BY table_name DESC;
